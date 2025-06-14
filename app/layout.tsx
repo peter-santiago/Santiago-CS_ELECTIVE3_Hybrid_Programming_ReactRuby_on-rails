ce058e189845203aff7199899871fb03fb5f8b21
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-06-14 21:15:28 +0800

    yipi update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,26 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
- 
+import React from 'react';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">  
-    <header className="sticky top-0 z-50 bg-gray-800 text-white p-4 text-center">
-    Hello World
-    </header>
-      <body className={`${inter.className} antialiased`}>{children}</body>
-      
-<footer className="sticky bottom-0 z-50 bg-gray-800 text-white p-4 text-center">
-  Hello World
-</footer>
+    <html lang="en">
+      <head />
+      <body className={`${inter.className} antialiased`}>
+        <header className="sticky top-0 z-50 bg-gray-800 text-white p-4 text-center">
+          Hello World
+        </header>
+
+        <main>{children}</main>
+
+        <footer className="sticky bottom-0 z-50 bg-gray-800 text-white p-4 text-center">
+          Hello World
+        </footer>
+      </body>
     </html>
   );
 }
